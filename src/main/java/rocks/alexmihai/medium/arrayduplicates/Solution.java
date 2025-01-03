52b22337cd636ab7e6bcafc7ea59aeea780c6a7d
Alex Mihai <49410241+alexmihai1984@users.noreply.github.com>
2025-01-03 12:52:06 +0200

    442. Find All Duplicates in an Array - solution explanation

--- src/main/java/rocks/alexmihai/medium/arrayduplicates/Solution.java
+++ src/main/java/rocks/alexmihai/medium/arrayduplicates/Solution.java
@@ -3,6 +3,17 @@ package rocks.alexmihai.medium.arrayduplicates;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * - Use some non-positive values to mark state:
+ *  - 0 - value deleted, so as not to be picked up again
+ *  - -1 - value `index + 1` has been seen before
+ * - Iterate elements and select the first index with positive value (first element the first time)
+ * - Set the value to 0, so as not to be picked up again
+ * - If the value at index `value - 1` is -1, add `value` to results
+ * - Set the value at index `value - 1` to -1
+ * - Use the previously existing value at index `value - 1` as the next value, else it will be lost; but if it's a
+ *   non-positive value, pick the next positive one from the array
+ */
 public class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         var res = new ArrayList<Integer>();
@@ -10,22 +21,21 @@ public class Solution {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] < 0) continue;
 
-            var currentValue = nums[i];
+            var val = nums[i];
             nums[i] = 0;
-            while (currentValue > 0) {
-                var idx = currentValue - 1;
+            while (val > 0) {
+                var idx = val - 1;
 
                 if (nums[idx] == -1) {
-                    res.add(currentValue);
-                    nums[idx] = -2;
+                    res.add(val);
                 }
 
-                if (idx == i) {
+                if (nums[idx] <= 0) {
                     nums[idx] = -1;
                     break;
                 }
 
-                currentValue = nums[idx];
+                val = nums[idx];
                 nums[idx] = -1;
             }
         }
--- src/main/java/rocks/alexmihai/medium/arrayduplicates/Solution2.java
+++ src/main/java/rocks/alexmihai/medium/arrayduplicates/Solution2.java
@@ -3,6 +3,13 @@ package rocks.alexmihai.medium.arrayduplicates;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * - Iterate array
+ * - If a particular value is negative, it means the value `index + 1` has been seen before and this is the second occurrence
+ * - Look at `nums[value - 1]`
+ *   - If it's negative, add `value` to results
+ *   - If it's positive, turn it negative, meaning `value` has been seen
+ */
 public class Solution2 {
     public List<Integer> findDuplicates(int[] nums) {
         var res = new ArrayList<Integer>();
