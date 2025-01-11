9e83f798a36db2585a5e350abbc46ca0238dfca0
Alex Mihai <49410241+alexmihai1984@users.noreply.github.com>
2025-01-11 21:22:04 +0200

    99. Recover Binary Search Tree

--- src/main/java/rocks/alexmihai/medium/recoverbst/Solution.java
+++ src/main/java/rocks/alexmihai/medium/recoverbst/Solution.java
@@ -2,6 +2,18 @@ package rocks.alexmihai.medium.recoverbst;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * During in-order traversal, the elements are supposed to be in order, but the rule will be broken once or twice.
+ * Define `first` to be the reference to the first element out of position.
+ * Define `second` to be the reference to the second element out of position.
+ * Traverse the tree in-order. During traversal, always keep track of the previously visited node `prev`.
+ * When `prev` > current, it means the 2 nodes are out of order. Save `prev` in `first` and the current node in
+ * `second`. This covers the case when the out-of-order nodes are consecutive during traversal.
+ * Continue traversing and if there's a second pair that is out of order, save the current node in `second`. It means
+ * the greater element in the first pair and the smaller element in the second pair are the ones that need to be
+ * swapped. Basically if you think of all the elements listed in-order, in the 2 pairs that are out of order you want
+ * to put the lef-most element at the end and the right-mos element at the start.
+ */
 public class Solution {
     public void recoverTree(TreeNode root) {
         AtomicReference<TreeNode> first = new AtomicReference<>();
