a47ea9cf761667d31bf5429da834928fdcb8c726
Alex Mihai <alexmihai1984@gmail.com>
2025-01-20 21:41:29 +0200

    95. Unique Binary Search Trees II

--- src/main/java/rocks/alexmihai/medium/uniquebst/Solution.java
+++ src/main/java/rocks/alexmihai/medium/uniquebst/Solution.java
@@ -3,9 +3,16 @@ package rocks.alexmihai.medium.uniquebst;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Let's think of the tree at the root.
+ * - Any element can be the root
+ * - Given a chosen root element, the unique trees are all combinations of all possible left and right subtrees,
+ *   taking into consideration the ranges (less than root on the left, greater than root on the right).
+ * This is recursive, as it applies for subtrees also.
+ * In theory, we could cache subtrees, but in practice the measured perf is worse.
+ */
 public class Solution {
     public List<TreeNode> generateTrees(int n) {
-
         var res = new ArrayList<TreeNode>();
         for (int i = 1; i <= n; i++) {
             res.addAll(generateAllTrees(new TreeNode(i), 1, n));
